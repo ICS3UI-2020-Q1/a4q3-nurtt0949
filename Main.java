ad3f55a16fcda15ff25cbb29f1cd4b67efe90a0d
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 15:11:21 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *Calculate sum from 1 to users number
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +11,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create a scanner
+    Scanner input = new Scanner(System.in);
+   //ask user for an integer to count down 
+    System.out.println("Please enter an integer to count down to from");
+    //declare a variable
+    int integer = input.nextInt();
+    //declare a variable to keep track of what number we are on
+    int count = 1;
+    //accumulator to add the count into it
+    int total = 0;
+    while(count <= integer){
     
-  }
-}
+    //add the numbers as we go into total
+    total = total + count;
+    //add 1 to our count to increase it 
+    count = count + 1;
+    }
+    //print total on the screen
+    System.out.println("The sum from 1 - " + integer + " is " + total);
+
+
+  
+    
+    
+
+		
+     }
+}  
+  
